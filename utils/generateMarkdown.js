2f658b4aeed3cf4e3be520af2c10ca1c1e65d4d2
JioR95 <jiovani.rivera@rocketmail.com>
2022-10-10 17:27:45 -0400

    update

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -17,4 +17,20 @@ const renderLicenseBadge = license => {
   // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
 const renderLicenseLink = license => {
-   
+    if (license == 'GPLv2') {  
+      return '(https://www.gnu.org/licenses/old-licenses/gpl-2.0.en.html)';
+  } else if (license == 'Apache 2.0') {
+    return `(https://opensource.org/licenses/Apache-2.0)`;
+  } else if (license == 'MIT') {
+    return `(https://opensource.org/licenses/MIT)`;
+  } else if (license == 'No License') {
+      return `(https://choosealicense.com/no-permission/)`;
+    } else {
+      return '';
+    }
+  };
+
+  // TODO: Create a function that returns the license section of README
+// If there is no license, return an empty string
+function renderLicenseSection(license) { 
+ 
